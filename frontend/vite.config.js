947d60d0f50e845fd788ef6ccc4d0509251b4932
shivam@123 <shivamraiprince085@gmail.com>
2026-06-08 15:50:08 +0530

    Update Module Federation config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,24 +3,46 @@ import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
 
 export default defineConfig({
+  base: "/",
+
   plugins: [
     react(),
     federation({
       name: "asset_management",
       filename: "remoteEntry.js",
       exposes: {
-        "./AssetApp": "./src/App.jsx"
+        "./AssetApp": "./src/App.jsx",
       },
-      shared: ["react", "react-dom", "react-router-dom"]
-    })
+      shared: {
+        react: {
+          singleton: true,
+          requiredVersion: false,
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: false,
+        },
+        "react-router-dom": {
+          singleton: true,
+          requiredVersion: false,
+        },
+      },
+    }),
   ],
+
   server: {
-    port: 5173
+    port: 5173,
+    cors: true,
   },
+
   preview: {
-    port: 5001
+    port: 5001,
+    strictPort: true,
+    cors: true,
   },
+
   build: {
-    target: "esnext"
-  }
+    target: "esnext",
+    cssCodeSplit: false,
+  },
 });
