6943c5c2e28ff6f565fcabb717f493937fc7d685
shivam@123 <shivamraiprince085@gmail.com>
2026-06-08 13:55:20 +0530

    Replace old migration with new InitialCreate

--- backend/Migrations/20260608082140_InitialCreate.Designer.cs
+++ backend/Migrations/20260608082140_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace AssetManagementSystem.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260608080954_InitialCreate")]
+    [Migration("20260608082140_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
