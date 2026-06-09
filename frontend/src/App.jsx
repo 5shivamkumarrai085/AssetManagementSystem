fb2b7ad72a73cdd9f4ba8a5384136535f88b7666
shivam@123 <shivamraiprince085@gmail.com>
2026-06-09 16:47:21 +0530

    Updated Asset Management System

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,20 @@
-import { Navigate, Route, Routes, BrowserRouter } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import Assets from "./pages/Assets.jsx";
 import Employees from "./pages/Employees.jsx";
 import Assignments from "./pages/Assignments.jsx";
 
-function AppContent() {
+export default function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/assets" element={<Assets />} />
-        <Route path="/employees" element={<Employees />} />
-        <Route path="/assignments" element={<Assignments />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route index element={<Dashboard />} />
+        <Route path="assets" element={<Assets />} />
+        <Route path="employees" element={<Employees />} />
+        <Route path="assignments" element={<Assignments />} />
+        <Route path="*" element={<Navigate to="" replace />} />
       </Routes>
     </Layout>
-  );
-}
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <AppContent />
-    </BrowserRouter>
   );
 }
